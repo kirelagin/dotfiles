492307d70dfbd31f4e71afb21b26374ede8fbb6c
Kirill Elagin <kirelagin@gmail.com>
2016-10-07 18:06:52 +0300

    zkbd: fix it for macOS

--- modules/zsh/kbd.zsh
+++ modules/zsh/kbd.zsh
@@ -1,6 +1,7 @@
 ###
 # !!! You have to run "autoload -U zkbd && zkbd" first
-source ~/.zkbd/$TERM-${DISPLAY:-$VENDOR-$OSTYPE}
+local termID=${${DISPLAY:t}:-$VENDOR-$OSTYPE}
+source ~/.zkbd/$TERM-$termID
 
 bindkey -v  # vim mode
 bindkey -M viins '^x' push-line
