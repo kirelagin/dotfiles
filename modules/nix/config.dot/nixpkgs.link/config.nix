584b5e905a35e5b17cc85e2762a1458bdd9ea470
Kirill Elagin <kirelagin@gmail.com>
2017-07-13 14:39:35 +0300

    nixpkgs config: clean up code

--- modules/nix/config.dot/nixpkgs.link/config.nix
+++ modules/nix/config.dot/nixpkgs.link/config.nix
@@ -1,7 +1,6 @@
 {
-  packageOverrides = pkgs_: let pkgs = pkgs_.pkgs; in with pkgs; {
 
-    withMan = lib.concatMap (p: [p] ++ lib.optional (p ? man) p.man);
+  packageOverrides = pkgs_: let pkgs = pkgs_.pkgs; in with pkgs; {
 
     all = with pkgs; buildEnv {
       name = "userPackages";
@@ -9,9 +8,11 @@
                 thisDir = builtins.toPath ./.;
                 packageFiles = builtins.attrNames (lib.filterAttrs (n: t: t == "regular" && lib.hasPrefix "packages" n) (builtins.readDir thisDir));
                 packageSets = map (n: import (builtins.toPath (thisDir + "/" + n)) { pkgs_ = pkgs_; pkgs = pkgs; } ) packageFiles;
-                hereSets =  builtins.filter (s: (!(s ? condition)) || s.condition) packageSets;
+                hereSets =  builtins.filter (s: s.condition or true) packageSets;
                 userPackages = lib.concatMap (s: s.userPackages) hereSets;
-              in withMan userPackages;
+              in userPackages;
+      extraOutputsToInstall = [ "man" ];
+      pathsToLink = [ "/bin" "/sbin" "/share" ];
     };
   };
 }
