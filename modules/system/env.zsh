3031270b11277ea9b20f7ff9490d7a4244bd8ee4
Kirill Elagin <kirelagin@gmail.com>
2019-10-28 20:26:31 +0100

    Impove EDITOR detection

--- modules/system/env.zsh
+++ modules/system/env.zsh
@@ -1,1 +1,8 @@
-(expr '(' "$EDITOR" : ".*vim" ')' '=' 0 && which vim) >/dev/null && export EDITOR=vim
+expr '(' "$EDITOR" : ".*vim" ')' '=' 0 >/dev/null && {
+  if command -v nvim >/dev/null; then
+    EDITOR=nvim
+  elif command -v vim >/dev/null; then
+    EDITOR=vim
+  fi
+  export EDITOR
+}
